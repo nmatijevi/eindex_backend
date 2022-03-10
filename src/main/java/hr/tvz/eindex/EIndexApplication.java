050f21901e82315c59af688e2d27ec18a9619750
Nikola <matijevicnikola@yahoo.com>
2022-03-10 18:39:11 +0100

    Prazan projekt - initial commit

---
+++ src/main/java/hr/tvz/eindex/EIndexApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.eindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/eindex/EIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.eindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
